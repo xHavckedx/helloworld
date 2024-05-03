53f3a172204e4d7b1093ff296b7246e8f446de09
xHavckedx <leocanogomez00@outlook.com>
2024-05-03 17:51:08 +0200

    feat(rest): Adding new test to rest

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -35,5 +35,24 @@ class TestApi(unittest.TestCase):
             response.read().decode(), "8", "ERROR SQRT"
         )
 
+    def test_api_multiply(self):
+        url = f"{BASE_URL_MOCK}/calc/multiply/2/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "4", "ERROR MULTIPLY"
+        )
+    
+    def test_api_divide(self):
+        url = f"{BASE_URL_MOCK}/calc/divide/2/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "1", "ERROR MULTIPLY"
+        )
 if __name__ == "__main__":  # pragma: no cover
     unittest.main()
