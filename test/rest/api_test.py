aac4009d2aca1f05a1d1434726d12d13b10c4ee6
xHavckedx <leocanogomez00@outlook.com>
2024-05-03 17:54:50 +0200

    fix: fixing divide function

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -52,7 +52,7 @@ class TestApi(unittest.TestCase):
             response.status, http.client.OK, f"Error en la petición API a {url}"
         )
         self.assertEqual(
-            response.read().decode(), "1", "ERROR MULTIPLY"
+            response.read().decode(), "1.0", "ERROR DIVIDE"
         )
 if __name__ == "__main__":  # pragma: no cover
     unittest.main()
